a6202f373e5b00c0950d2f30363e8b0ca8aa7ce0
Fahmi Idris <fahmiidris.1607@gmail.com>
2021-09-06 09:48:23 +0700

    Update Tugas 6 – Javascript ES6

---
+++ Tugas-Harian-Part-1/Tugas-6/tugas6.js
@@ -0,0 +1,105 @@
+console.log("\n===== Soal 1 =====")
+// Soal 1
+// Luas lingkaran
+const default_phi = 22 / 7
+const luasLingkaran = (jari_jari, phi = default_phi) => {
+    const diameter = jari_jari ** 2
+    const luas = phi * diameter
+    return luas
+}
+
+// keliling lingkaran
+const kelilingLingkaran = (jari_jari, phi = default_phi) => {
+    const keliling = 2 * phi * jari_jari
+    return keliling
+}
+
+// Eksekusi
+const hasilLuas = luasLingkaran(7)
+const hasilKeliling = kelilingLingkaran(7)
+
+console.log(hasilLuas)
+console.log(hasilKeliling)
+
+
+
+
+console.log("\n===== Soal 2 =====")
+// Soal 2
+const introduce = (...params) => {
+    const [name, age, gender, hobby] = params
+    const hasil = gender === "Laki-Laki" ? `Pak ${name} adalah seorang ${hobby} yang berusia ${age} tahun` : `Ops Salah Gender!`
+    return hasil
+}
+
+// Ketentuan output
+// "Pak John adalah seorang penulis yang berusia 30 tahun"
+
+const perkenalan = introduce("John", "30", "Laki-Laki", "penulis")
+console.log(perkenalan) // Menampilkan "Pak John adalah seorang penulis yang berusia 30 tahun"
+
+
+
+
+console.log("\n===== Soal 3 =====")
+// Soal 3
+const newFunction = (firstName, lastName) => {
+    return {
+        firstName,
+        lastName,
+        fullName: () => {
+            console.log(firstName + " " + lastName)
+        }
+    }
+}
+
+// kode di bawah ini jangan diubah atau dihapus sama sekali
+console.log(newFunction("John", "Doe").firstName)
+console.log(newFunction("Richard", "Roe").lastName)
+newFunction("William", "Imoh").fullName()
+
+
+
+
+console.log("\n===== Soal 4 =====")
+// Soal 4
+let phone = {
+    name: "Galaxy Note 20",
+    brand: "Samsung",
+    year: 2020,
+    colors: ["Mystic Bronze", "Mystic White", "Mystic Black"]
+}
+
+const { name, brand, year, colors } = phone
+const phoneBrand = brand
+const phoneName = name
+const colorBlack = colors[2]
+const colorBronze = colors[0]
+
+console.log(phoneBrand, phoneName, year, colorBlack, colorBronze)
+
+
+
+
+console.log("\n===== Soal 5 =====")
+// Soal 5
+let warna = ["biru", "merah", "kuning", "hijau"]
+
+let dataBukuTambahan = {
+    penulis: "john doe",
+    tahunTerbit: 2020
+}
+
+let buku = {
+    nama: "pemograman dasar",
+    jumlahHalaman: 172,
+    warnaSampul: ["hitam"]
+}
+
+buku.warnaSampul = [...buku.warnaSampul, ...warna]
+const combineBuku = {
+    ...buku,
+    ...dataBukuTambahan,
+}
+
+console.log(combineBuku)
