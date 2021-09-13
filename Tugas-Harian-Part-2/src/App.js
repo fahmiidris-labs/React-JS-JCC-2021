697e5de260962f504261fdcc723a8b1a205b61d4
Fahmi Idris <fahmiidris.1607@gmail.com>
2021-09-13 12:38:05 +0700

    Update Tugas 11 – CRUD Reactjs ( With Hooks )

--- Tugas-Harian-Part-2/src/App.js
+++ Tugas-Harian-Part-2/src/App.js
@@ -1,14 +1,16 @@
 import React from "react"
 import "./Styles/App.css"
 
-import Tugas9 from "./Tugas-9/tugas-9"
-import Tugas10 from "./Tugas-10/tugas-10"
+// import Tugas9 from "./Tugas-9/tugas-9"
+// import Tugas10 from "./Tugas-10/tugas-10"
+import Tugas11 from "./Tugas-11/tugas-11"
 
 const App = () => {
     return (
         <>  
-            <Tugas10 />
-            <Tugas9 />
+            <Tugas11 />
+            {/* <Tugas10 /> */}
+            {/* <Tugas9 /> */}
         </>
     )
 }
---
+++ Tugas-Harian-Part-2/src/Tugas-11/tugas-11.js
@@ -0,0 +1,129 @@
+import React, { useState } from 'react'
+import "../Styles/Tugas-11.css"
+
+const Tugas11 = () => {
+
+    const initialState = [
+        { nama: "Nanas", hargaTotal: 100000, beratTotal: 4000 },
+        { nama: "Manggis", hargaTotal: 350000, beratTotal: 10000 },
+        { nama: "Nangka", hargaTotal: 90000, beratTotal: 2000 },
+        { nama: "Durian", hargaTotal: 400000, beratTotal: 5000 },
+        { nama: "Strawberry", hargaTotal: 120000, beratTotal: 6000 }
+    ]
+
+    const initialCrendentials = {
+        nama: "",
+        hargaTotal: "",
+        beratTotal: 0
+    }
+
+    const [buah, setBuah] = useState(initialState)
+    const [credentials, setCredentials] = useState(initialCrendentials)
+    const [currentIndex, setCurrentIndex] = useState(-1)
+
+    const onHandleChange = async (event) => {
+        setCredentials(old => ({
+            ...old,
+            [event.target.name]: event.target.value
+        }))
+    }
+
+    const onSubmitHandle = async (event) => {
+        event.preventDefault()
+
+        let newData = buah
+
+        if (currentIndex === -1) {
+            newData = [...buah, credentials]
+        } else {
+            newData[currentIndex] = credentials
+        }
+
+        setBuah(newData)
+        setCredentials(initialCrendentials)
+        setCurrentIndex(-1)
+    }
+
+    const onDeleteHandle = (event) => {
+        let index = parseInt(event.target.value)
+        let deletedItem = buah[index]
+        let newData = buah.filter((e) => { return e !== deletedItem })
+        setBuah(newData)
+    }
+
+    const onEditHandle = (event) => {
+        let index = parseInt(event.target.value)
+        let edit = buah[index]
+        setCredentials(edit)
+        setCurrentIndex(event.target.value)
+    }
+
+    return (
+        <div className="box-table">
+            <div>
+                <h2 align="center">
+                    Daftar Harga Buah
+                </h2>
+                <table className="products-table">
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>Nama</th>
+                            <th>Harga Total</th>
+                            <th>Berat Total</th>
+                            <th>Harga Per Kg</th>
+                            <th>Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {buah.map((data, index) => (
+                            <tr key={index}>
+                                <td>{index + 1}</td>
+                                <td>{data.nama}</td>
+                                <td>Rp.{data.hargaTotal}</td>
+                                <td>{data.beratTotal / 1000} Kg</td>
+                                <td>Rp.{data.hargaTotal / (data.beratTotal / 1000)}</td>
+                                <td>
+                                    <button type="button" onClick={onEditHandle} value={index}>
+                                        Edit
+                                    </button>
+                                    <button type="button" onClick={onDeleteHandle} value={index}>
+                                        Hapus
+                                    </button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+
+                <br />
+                <br />
+
+                <h2 align="center">
+                    Form Daftar Harga Buah
+                </h2>
+                <form onSubmit={onSubmitHandle} className="form">
+                    <div className="input-group">
+                        <label htmlFor="nama">Nama</label>
+                        <input id="nama" type="text" name="nama" value={credentials.nama} onChange={onHandleChange} required />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="hargaTotal">Harga Total</label>
+                        <input id="hargaTotal" type="text" name="hargaTotal" value={credentials.hargaTotal} onChange={onHandleChange} required />
+                    </div>
+
+                    <div className="input-group">
+                        <label htmlFor="beratTotal">Berat Total (dalam satuan gram)</label>
+                        <input id="beratTotal" type="number" name="beratTotal" value={credentials.beratTotal} onChange={onHandleChange} required min={2000} />
+                    </div>
+
+                    <button type="submit">
+                        Submit
+                    </button>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Tugas11
